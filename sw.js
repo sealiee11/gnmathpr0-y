de7b87f65ab130de988b597cd1c201a0d24f4a2e
sealiee1 <159606347+sealiee11@users.noreply.github.com>
2026-03-24 11:26:20 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,12 @@
 const CDN = "https://cdn.jsdelivr.net/gh/sealiee11/gnmathpr0-y@main/";
-importScripts(CDN + "stuf/uv.bundle.js", CDN + "stuf/sw.js");
+importScripts(CDN + "stuf/uv.bundle.js");
+self.__uv$config = {
+    prefix: "./stuf/service/",
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: CDN + "stuf/uv.handler.js",
+    bundle: CDN + "stuf/uv.bundle.js",
+    config: CDN + "stuf/uv.config.js",
+    sw: CDN + "stuf/sw.js",
+};
+importScripts(CDN + "stuf/sw.js");
