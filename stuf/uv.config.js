8d13869ebeae997b480568de042e1850ad9473f9
sealiee1 <159606347+sealiee11@users.noreply.github.com>
2026-03-31 16:17:49 -0700

    Update uv.config.js

--- stuf/uv.config.js
+++ stuf/uv.config.js
@@ -1,11 +1,11 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: "./stuf/service/",
+    prefix: "stuf/service/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "./stuf/uv.handler.js",
-    client: "./stuf/uv.client.js",
-    bundle: "./stuf/uv.bundle.js",
-    config: "./stuf/uv.config.js",
-    sw: "./stuf/uv.sw.js",
+    handler: "stuf/uv.handler.js",
+    client: "stuf/uv.client.js",
+    bundle: "stuf/uv.bundle.js",
+    config: "stuf/uv.config.js",
+    sw: "stuf/uv.sw.js",
 };
