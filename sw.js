e0dc6f7c6273befd67b4c133a206974798c45c68
sealiee1 <159606347+sealiee11@users.noreply.github.com>
2026-04-12 14:34:20 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 importScripts('https://cdn.jsdelivr.net/gh/sealiee11/gnmathpr0-y@main/stuf/uv.bundle.js');
 importScripts('https://cdn.jsdelivr.net/gh/sealiee11/gnmathpr0-y@main/stuf/uv.config.js');
-importScripts('https://cdn.jsdelivr.net/gh/sealiee11/gnmathpr0-y@main/stuf/uv.sw.js');
+importScripts('https://cdn.jsdelivr.net/gh/sealiee11/gnmathpr0-y@main/stuf/sw.js');
